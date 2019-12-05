156de87992fc78081145c7549cf4d2dc8e0b9eea
ksukhaldun <kseniya.khaldun@gmail.com>
2019-12-05 16:00:44 +0300

    Updates

---
+++ precache-manifest.25e126b13bff41402891d4a939c3e281.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b80e9ff860f888ef956dd502989e21d",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "ecefa7728f2e7b491357",
+    "url": "/react_static-list-of-posts/static/css/main.a84d296d.chunk.css"
+  },
+  {
+    "revision": "d1bf606ea68b30240b50",
+    "url": "/react_static-list-of-posts/static/js/2.a7975bab.chunk.js"
+  },
+  {
+    "revision": "ecefa7728f2e7b491357",
+    "url": "/react_static-list-of-posts/static/js/main.3b4b9f5c.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
